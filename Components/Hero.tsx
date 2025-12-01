acd75cac72517667bfeb484aedf2b667b659307d
Chidelemmanuel <emmanuelozochi2019@gmail.com>
2025-12-01 10:01:17 +0100

    hero section built successfully

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -3,16 +3,15 @@ import Passport from "@/public/passport.jpg";
 
 export default function Hero() {
     return (
-        <div className="flex flex-col items-center justify-center text-center h-screen bg-[#000000] text-white px-4  2xl:w-[60%] mx-auto">
-            <div className=""> 
-                <h4 className="lg:text-5xl text-[#fcfc] font-bold"> Hello, my name is</h4>
-                <h1 className="lg:text-6xl font-extrabold">Ozochi <span className="text-red-500">Emmanuel</span></h1>
-                <h3 className="lg:text-3xl font-bold">I am a <span className="text-red-500"> Frontend</span> Engineer.</h3>
-                
+        <div className="flex flex-col items-center justify-center text-center h-130 bg-[#000000] text-white px-4  lg:flex-row lg:justify-around 2xl:w-[60%] mx-auto">
+            <div className="flex flex-col items-center my-15"> 
+                <h4 className="text-2xl lg:text-5xl text-[#fcfc] font-bold"> Hello, my name is</h4>
+                <h1 className="text-3xl lg:text-6xl font-extrabold">Ozochi <span className="text-red-500">Emmanuel</span></h1>
+                <h3 className="text-2xl lg:text-3xl font-bold">I am a <span className="text-red-500"> Frontend</span> Engineer.</h3>
             </div>
 
-            <span className="hidden lg: 2xl:">
-                <Image src={Passport} alt="Passport" /> 
+            <span className="flex flex-col">
+                <Image src={Passport} alt="Passport" className="rounded-full w-[300px] lg:w-[400px]" /> 
             </span>
 
             {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
